8c63b16448e08c8d3bbb55124f60679e85096b3a
Adam Drago <atdrago@gmail.com>
2022-04-21 16:48:40 -0400

    feat: Update contact page and favicon

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,27 +8,15 @@ const Home: NextPage = () => {
       <main className="prose lg:prose-xl">
         <section>
           <p className="text-lg">
-            Whether you&apos;re looking for someone to evaluate your current
-            website&apos;s situation and help with updates or optimizations, or
-            you&apos;re looking to build something completely from scratch, I
-            may be interested!
-          </p>
-          <p className="text-lg">
-            I&apos;m currently in search of my next opportunity, and I&apos;ll
-            update this page when I&apos;m not. Vegan companies and companies
-            looking to <b className="text-bold">do good</b> will be considered
-            above all others.
-          </p>
-          <p className="text-lg">
-            Please email{" "}
+            Please email me at{" "}
             <b className="text-bold">
               <a href="mailto:atdrago+business@gmail.com">
                 atdrago+business@gmail.com
               </a>
             </b>{" "}
-            for all inquiries. Thanks!
+            with any questions or inquiries. Thanks!
           </p>
-          <p className="text-lg">✌🏻🖤</p>
+          <p className="text-lg">✌🏻 🖤</p>
         </section>
       </main>
     </div>
