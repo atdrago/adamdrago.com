f5365be3ada03eb47bcab426800825fd79583cae
Adam Drago <atdrago@gmail.com>
2022-08-18 08:22:30 -0400

    chore(deps): Remove @capsizecss/core and @capsizecss/metrics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+/**
+ * Document is only rendered on the server side, NOT on the client side
+ */
 export default function Document() {
   return (
     <Html>
