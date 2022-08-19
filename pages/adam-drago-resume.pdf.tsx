193da959cad0d17c68e4504faa0c2df722fe7649
Adam Drago <atdrago@gmail.com>
2022-08-19 13:26:05 -0400

    use @BetaHuhn's getPdf

--- pages/adam-drago-resume.pdf.tsx
+++ pages/adam-drago-resume.pdf.tsx
@@ -1,40 +1,9 @@
 import type { GetServerSideProps } from "next";
-import chromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer";
 
-const getBrowserInstance = async (): Promise<
-  Awaited<
-    ReturnType<typeof chromium.puppeteer.launch | typeof puppeteer.launch>
-  >
-> => {
-  const executablePath = await chromium.executablePath;
-
-  if (!executablePath) {
-    return await puppeteer.launch({
-      args: chromium.args,
-      headless: true,
-    });
-  }
-
-  return await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-};
+import { getPdf } from "lib/server/getPdf";
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const browser = await getBrowserInstance();
-
-  const page = await browser.newPage();
-
-  await page.goto(process.env.RESUME_URL, { waitUntil: "networkidle0" });
-
-  const pdf = await page.pdf({ format: "a4" });
-
-  await browser.close();
+  const pdf = await getPdf(process.env.RESUME_URL);
 
   res.setHeader("Content-Type", "application/pdf; charset=utf-8");
   res.setHeader("Content-Length", pdf.length);
