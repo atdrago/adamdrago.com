c008bfef124283db6435750d043192bf949adeda
Adam Drago <atdrago@gmail.com>
2022-08-22 17:19:21 -0400

    feat(work): Add download links to top of work page

--- pages/work.tsx
+++ pages/work.tsx
@@ -7,6 +7,8 @@ const updatedAt = Intl.DateTimeFormat([], { dateStyle: "full" })
   .format(new Date(resumeData.updatedAt))
   .toString();
 
+const updatedAtSearchParam = resumeData.updatedAt.replace(/\//g, "-");
+
 export default function WorkPage() {
   return (
     <div className="flex flex-col gap-2">
@@ -14,7 +16,20 @@ export default function WorkPage() {
       <main className="prose lg:prose-xl dark:prose-invert">
         <aside className="print:hidden">
           <p className="bg-neutral-50 dark:bg-neutral-900 border border-neutral-100 dark:border-neutral-800 px-3 py-2 text-lg transition-colors">
-            The following is my resumé, updated as of <b>{updatedAt}</b>.
+            The following is my resumé, updated as of <b>{updatedAt}</b>. It is
+            also available to{" "}
+            <Link href={`/adam-drago-resume.pdf?date=${updatedAtSearchParam}`}>
+              <a download="adam-drago-resume.pdf" target="_blank">
+                download as a PDF
+              </a>
+            </Link>{" "}
+            or{" "}
+            <Link href={`/adam-drago-resume.txt?date=${updatedAtSearchParam}`}>
+              <a download="adam-drago-resume.txt" target="_blank">
+                text file
+              </a>
+            </Link>{" "}
+            if that is more convenient.
           </p>
         </aside>
         {resumeData.sections.map(({ heading, items }, index) => {
@@ -88,7 +103,7 @@ export default function WorkPage() {
             Downloads
           </h2>
           <div className="flex items-start gap-6">
-            <Link href="/adam-drago-resume.txt">
+            <Link href={`/adam-drago-resume.txt?date=${updatedAtSearchParam}`}>
               <a
                 className="rounded-lg p-3 pr-4 bg-sky-500 text-white font-bold no-underline flex gap-2 items-center"
                 download="adam-drago-resume.txt"
@@ -97,7 +112,7 @@ export default function WorkPage() {
                 <FileArrowDown size={32} /> TXT
               </a>
             </Link>
-            <Link href="/adam-drago-resume.pdf">
+            <Link href={`/adam-drago-resume.pdf?date=${updatedAtSearchParam}`}>
               <a
                 className="rounded-lg p-3 pr-4 bg-sky-500 text-white font-bold no-underline flex gap-2 items-center"
                 download="adam-drago-resume.pdf"
